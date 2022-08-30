c801ecbabbb24ea3061059ce3060c54cbeaa64c5
Trí Đức Vũ <triduc1204@gmail.com>
2022-08-30 09:56:59 +0700

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://hoangan.edu.vn">Hoàng Ân Edu!</a>
         </h1>
 
         <p className={styles.description}>
